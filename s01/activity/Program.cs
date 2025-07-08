64d011cedfc89d6023ac820a05d87876d011905c
arvinmcaparros <arvinmcaparros@gmail.com>
2025-07-08 11:14:03 +0800

    initial commit

---
+++ s01/activity/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}! Welcome to .NET");
+
+Console.ReadLine(); 
