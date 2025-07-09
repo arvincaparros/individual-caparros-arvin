f52bd641d36a6dabe9cde7be2a3d878182d87765
arvinmcaparros <arvinmcaparros@gmail.com>
2025-07-09 15:11:52 +0800

    Update activity s03

--- s03/activity/NexusScm/NexusScm/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NexusScm.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
