c0e5435596c0d1c4e858145d8b7fcddc50a88e4d
arvinmcaparros <arvinmcaparros@gmail.com>
2025-07-16 11:36:35 +0800

    Add activity code s08

---
+++ s08/discussion/NexusScm/Nexus.Core/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        // Foreign Key
+        public int SupplierId { get; set; }
+
+        public Supplier Supplier { get; set; }
+    }
+}
