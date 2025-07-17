904ef0915a3d51a0a77c16148e249785a29536c4
Arvs <arvinmcaparros@gmail.com>
2025-07-17 00:30:31 -0400

    Add discussion code s09

---
+++ s09/discussion/NexusScm/Nexus.API/Program.cs
@@ -0,0 +1,69 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Nexus.API.Data;
+using Nexus.API.Middleware;
+using Nexus.API.Services;
+using Nexus.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"
+],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+builder.Services.AddScoped<ITokenServices, TokenService>();
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionHandler>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ s09/discussion/NexusScm/Nexus.Web/Controllers/SuppliersController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nexus.Core;
+using Nexus.Web.Data;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Nexus.Web.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        //private readonly ApplicationDbContext _context;
+
+        //public SuppliersController(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SuppliersController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //var suppliers = await _context.Suppliers.ToListAsync();
+
+         
+
+            var client = _httpClientFactory.CreateClient("NexusApiClient");
+            var response = await client.GetAsync("api/suppliers");
+
+            List<Supplier> suppliers = new();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStream = await response.Content.ReadAsStreamAsync();
+                suppliers = await JsonSerializer.DeserializeAsync<List<Supplier>>(responseStream, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+            }
+
+            return View(suppliers);
+        }
+
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var supplier = await _context.Suppliers.FirstOrDefaultAsync(m => m.Id == id);
+
+        //    if (supplier == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(supplier);
+        //}
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name, ContactPerson, Email")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = _httpClientFactory.CreateClient("NexusApiClient");
+                var token = HttpContext.Session.GetString("JWToken");
+
+                if (!string.IsNullOrEmpty(token))
+                {
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                }
+
+                var response = await client.PostAsJsonAsync("api/suppliers", supplier);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Failed to create supplier. API returned an error.");
+                }
+            }
+
+            return View(supplier);
+        }
+
+
+    }
+}
